d6e4317dd273caf120f5a16029fe5c04bb5c72d7
primula <primularussell@gmail.com>
2020-10-21 12:19:57 -0700

    updated event function

--- TheOdinProject/Etch-a-Sketch/index.js
+++ TheOdinProject/Etch-a-Sketch/index.js
@@ -9,20 +9,12 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div');
-        cell.mouseover = function() {
-            '#' + Math.floor(Math.random() * 16777215).toString(16);
-        };
+        cell.addEventListener('mouseover', function(event) {
+            let rcolor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            event.target.style.backgroundColor = rcolor;
+        });
         container.appendChild(cell).className = "grid-item";
     };
 };
-/*
-var gridItems = document.getElementsByClassName('grid-item');
-
-for (i = 0; i < gridItems.length; i++) {
-    gridItems[i].onmouseover = function() {
-        '#' + Math.floor(Math.random() * 16777215).toString(16);
-    };
-};
-*/
 
 makeRows(16, 16);
